c830fd5460216eab056fb1b926f512d895ad7e3d
Georgiy <olenchenko.h.m@nmu.one>
2022-11-18 16:37:58 +0200

    LR2

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 )
@@ -139,7 +140,7 @@ func sortLines(inputFilename string, outputFilename string, revers bool, sortByC
 		if err := os.Truncate(outputFilename, 0); err != nil {
 			log.Printf("Failed to truncate: %v", err)
 		}
-		//Додаємо Хедер, якщо задано флаг -r
+		//Додаємо Хедер, якщо задано флаг -h
 		if ignoreHeader {
 			addHeader(file)
 		}
@@ -168,53 +169,174 @@ func sortLines(inputFilename string, outputFilename string, revers bool, sortByC
 }
 
 /*
+Ітерація файлів у папці
+*/
+func iterate(path string) []string {
+	var files []string
+
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".csv" {
+			files = append(files, path)
+			//fmt.Printf("File Name: %s\n", info.Name())
+		}
+		return nil
+	})
+	if err != nil {
+		return nil
+	}
+	return files
+}
+
+func sortLinesFromManyFiles(files []string, outputFileName string, revers bool, sortByColumnNumber int, ignoreHeader bool) bool {
+
+	var sorted sort.StringSlice
+	var tempString string
+
+	for j := 0; j < len(files); j++ {
+		fileIn, err := os.Open(files[j])
+		if err != nil {
+			log.Fatalf("Не вдалося відкрити файл %s: %s", files[j], err)
+			return false
+		}
+		//Читання кожної строки файлу та формуємо їх в одну строку
+		reader := bufio.NewReader(fileIn)
+		i := 0
+
+		for {
+			line, _, err := reader.ReadLine()
+
+			// Якщо строки завершилися, перериваємо цикл читання
+			if err == io.EOF {
+				break
+			}
+
+			//Якщо потрібно ігнорувати першу строку, ігноруємо її
+			if ignoreHeader {
+				//if revers {
+				//
+				//} else {
+				if i != 0 {
+					tempString = tempString + string(line) + "\n"
+					//}
+				}
+
+			} else {
+				tempString = tempString + string(line) + "\n"
+			}
+			i++
+		}
+		//Закриття файлу
+		closeFile(fileIn)
+
+	}
+	sorted = strings.Split(tempString, "\n") // конвертуємо строку в Slice з роздільником \n
+
+	if revers {
+		//Якщо необхідно сортувати в зворотньому порядку, сортуємо в зворотньому порядку
+		sorted.Sort()
+		sort.Sort(sort.Reverse(sorted))
+	} else {
+		sorted.Sort()
+	}
+	//Відкриваємо файл, в який необхідно додати строки
+	file, err := os.OpenFile(outputFileName, os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Виникла помилка під час створення файлу: %s", err)
+	}
+	//Очищуємо файл від попереднього змісту
+	if err := os.Truncate(outputFileName, 0); err != nil {
+		log.Printf("Failed to truncate: %v", err)
+	}
+	//Додаємо Хедер, якщо задано флаг -h
+	if ignoreHeader {
+		addHeader(file)
+	}
+
+	//Записуємо кожну строку в файл
+	datawriter := bufio.NewWriter(file)
+	for _, data := range sorted {
+		if len(data) != 0 {
+			fmt.Println("Resulte:", data)
+			_, _ = datawriter.WriteString(data + "\n")
+		}
+	}
+	//Посилаємо текст з буферу в файл
+	err = datawriter.Flush()
+	if err != nil {
+		log.Fatalf("Виникла помилка під час запису відсортованих даних в файл. %s", err)
+		return false
+	}
+
+	closeFile(file)
+	return true
+	return true
+}
+
+/*
 Розпочати програму
 */
-func startProgram(inputFile string, outputFile string, f int, h bool, r bool) bool {
+func startProgram(inputFile string, outputFile string, f int, h bool, r bool, dirName string) bool {
 	for { //нескінченний цикл
 
 		var nameSt, groupSt, typeSt string //змінні з ім'ям, групою, видом навчання (контракт/бюджет)
 		var value int                      //для вибору щодо додавання нової строки
 		var fileExist, appendedString bool //існує файл filename (true false)
+		var listOfFiles []string
 
-		fmt.Println("Введіть строку в форматі \"Прізвище Група Вид_навчання(К чи Б)\":")
-		_, err := fmt.Scanln(&nameSt, &groupSt, &typeSt) //Введення строки
-		if err != nil {
-			log.Fatalln("Виникла помилка - ", err)
-		}
+		if dirName != "/" {
+			//Якщо задано ім'я папки
+			currentDirectory, err := os.Getwd()
+			if err != nil {
+				log.Fatal(err)
+			}
+			listOfFiles = iterate(currentDirectory + "/" + dirName)
 
-		fileExist = ifExistFile(inputFile) //перевірка на існування файлу
-		if fileExist {
-			appendedString = appendToFile(inputFile, nameSt+","+groupSt+","+typeSt) //додання строки в файл
-			if appendedString {
-				fmt.Println("Успішно додано строку в файл!")
-				fmt.Printf("Бажаєте додати ще строку? 1 - так, 0 - ні: ")
-				_, err = fmt.Scanln(&value)
-				if err != nil {
-					sortLines(inputFile, outputFile, r, 0, h)
-					log.Fatalf("Виникла помилка: %s", err)
-					return false
-				}
-				if value != 1 {
-					sortLines(inputFile, outputFile, r, 0, h)
-					fmt.Printf("Вихід з програми.")
-					return true
-				}
-			} else {
-				fmt.Printf("Не вдалося додати строку в файл %s.", inputFile)
-				return false
+			sortLinesFromManyFiles(listOfFiles, outputFile, r, 0, h)
+			return true
+		} else {
+			fmt.Println("Введіть строку в форматі \"Прізвище Група Вид_навчання(К чи Б)\":")
+			_, err := fmt.Scanln(&nameSt, &groupSt, &typeSt) //Введення строки
+			if err != nil {
+				log.Fatalln("Виникла помилка - ", err)
 			}
 
-		} else {
-			fmt.Printf("Не вдалося створити файл %s.", inputFile)
-			return false
+			fileExist = ifExistFile(inputFile) //перевірка на існування файлу
+			if fileExist {
+				appendedString = appendToFile(inputFile, nameSt+","+groupSt+","+typeSt) //додання строки в файл
+				if appendedString {
+					fmt.Println("Успішно додано строку в файл!")
+					fmt.Printf("Бажаєте додати ще строку? 1 - так, 0 - ні: ")
+					_, err = fmt.Scanln(&value)
+					if err != nil {
+						sortLines(inputFile, outputFile, r, 0, h)
+						log.Fatalf("Виникла помилка: %s", err)
+						return false
+					}
+					if value != 1 {
+						sortLines(inputFile, outputFile, r, 0, h)
+						fmt.Printf("Вихід з програми.")
+						return true
+					}
+				} else {
+					fmt.Printf("Не вдалося додати строку в файл %s.", inputFile)
+					return false
+				}
+
+			} else {
+				fmt.Printf("Не вдалося створити файл %s.", inputFile)
+				return false
+			}
 		}
+
 	}
 
 }
 
 func main() {
-	var inputFile, outputFile string
+	var inputFile, outputFile, dirName string
 	var number int
 	var h, r bool
 	flag.Bool("help", false, "Отримати довідку")
@@ -222,23 +344,52 @@ func main() {
 	flag.StringVar(&inputFile, "i", "test.csv", "Використовувати файл file-name як вхідний файл")
 	flag.StringVar(&outputFile, "o", "test.csv", "Використовувати файл file-name як результуючий файл")
 	flag.IntVar(&number, "f", 0, "Сортувати вхідні строки за стовпцем N")
+	flag.StringVar(&dirName, "d", "/", "dir-name - Задати вхідну папку")
 	flag.BoolVar(&r, "r", false, "Реверсне сортування даних")
 	flag.Parse()
 
 	if outputFile != "test.csv" {
 		fmt.Printf("Обрано результуючий файл %s\n", outputFile)
+
+		if _, err := os.Stat(outputFile); os.IsNotExist(err) {
+			if err := os.MkdirAll(filepath.Dir(outputFile), 0770); err != nil {
+				log.Fatalln("Помилка")
+			}
+			_, err := os.Create(outputFile)
+			if err != nil {
+				return
+			}
+
+		}
 	}
 	if inputFile != "test.csv" {
 		fmt.Printf("Обрано вхідний файл %s. Подальші операції виконуватимуться з ним.\n", outputFile)
+		if _, err := os.Stat(outputFile); os.IsNotExist(err) {
+			if err := os.MkdirAll(filepath.Dir(outputFile), 0770); err != nil {
+				log.Fatalln("Помилка")
+			}
+			_, err := os.Create(outputFile)
+			if err != nil {
+				return
+			}
+
+		}
 	}
-	if number != 0 {
-		fmt.Printf("Виконувати сортування строк за стовпцем №%d\n", number)
-	}
+	//if number != 0 {
+	//	fmt.Printf("Виконувати сортування строк за стовпцем №%d\n", number)
+	//}
 	if r != false {
 		fmt.Printf("Виконувати реверсне сортування даних\n")
 	}
 	if h != false {
 		fmt.Printf("Ігнорувати хедер під час сортування даних\n")
 	}
-	startProgram(inputFile, outputFile, number, h, r)
+	if dirName != "/" {
+		fmt.Printf("Задано вхідну папку %s\n", dirName)
+	}
+	if dirName != "/" && inputFile != "test.csv" {
+		log.Fatalln("Помилка! Необхідно задати лише один з параметрів -d або -i.")
+	}
+	startProgram(inputFile, outputFile, number, h, r, dirName)
+
 }
